a0c6ddd8e21e91b20593ea637dc9cd03761f3c32
David Amador <54488581+dsamador@users.noreply.github.com>
2022-07-06 14:16:50 -0500

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { DemoModule } from './pages/demo/demo.module';
+
 
 
 const routes: Routes = [
