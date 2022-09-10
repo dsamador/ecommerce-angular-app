e6d7b9c68e525c0c39abbeed763c39cd30360c1f
David Amador <54488581+dsamador@users.noreply.github.com>
2022-09-10 18:41:22 -0500

    data models

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,6 @@ export class AppComponent {
   title = 'ecommerce-angular-app';
   constructor(private fs: AngularFirestore){ }
   ngOnInit(){
-    this.fs.collection('test').snapshotChanges().subscribe( personas => {
-      console.log(personas.map( x => x.payload.doc.data()));
-    });
+
   }
 }
