aa0335b359edf708e70fe5128399adaa2031d579
David Amador <54488581+dsamador@users.noreply.github.com>
2022-10-20 14:38:53 -0500

    Comentando

--- src/app/store/dictionaries/dictionaries.effects.ts
+++ src/app/store/dictionaries/dictionaries.effects.ts
@@ -1,59 +1,83 @@
 import { Injectable } from '@angular/core';
-import { Effect, Actions, ofType, createEffect } from '@ngrx/effects';
-import { AngularFirestore, DocumentChangeAction } from '@angular/fire/firestore';
+import { Actions, ofType, createEffect } from '@ngrx/effects';
+import {
+  AngularFirestore,
+  DocumentChangeAction,
+} from '@angular/fire/firestore';
 import { Observable, of, zip } from 'rxjs';
-import {map, switchMap, catchError, take} from 'rxjs/operators'
-import { Dictionaries, Dictionary, Item, ControlItem } from './dictionaries.models';
+import { map, switchMap, catchError, take } from 'rxjs/operators';
+import {
+  Dictionaries,
+  Dictionary,
+  Item,
+  ControlItem,
+} from './dictionaries.models';
 
 import * as fromActions from './dictionaries.actions';
 
 type Action = fromActions.All;
 
+/* metodo para mapear el documento de firebase a un item */
 const documentToItem = (x: DocumentChangeAction<any>): Item => {
+  /* El id no viene aca */
   const data = x.payload.doc.data();
-  return {
+  return {/* El id toca buscarlo desde aca */
     id: x.payload.doc.id,
-    ...data
-  }
-}
+    ...data,/* finalmente juntamos todo */
+  };
+};
 
-const itemToControlItem = (x:Item): ControlItem => ({
+const itemToControlItem = (x: Item): ControlItem => ({
   value: x.id,
   label: x.name,
-  icon: x.icon
-})
+  icon: x.icon,
+});
 
 const addDictionary = (items: Item[]): Dictionary => ({
   items,
-  controlItems: [...items].map(x=>itemToControlItem(x))
-})
+  controlItems: [...items].map((x) => itemToControlItem(x)),
+});
 
 @Injectable()
 export class DictionariesEffects {
-  constructor(private actions: Actions, private afs: AngularFirestore){}
-  read: Observable<Action> = createEffect(()=>
+
+  constructor(private actions: Actions, private afs: AngularFirestore) {}
+
+  read: Observable<Action> = createEffect(() =>
     this.actions.pipe(
       ofType(fromActions.Types.READ),
-      switchMap( ()=> {
+      switchMap(() => {
         return zip(
-          this.afs.collection('roles').snapshotChanges().pipe(
-            take(1),
-            map( items => items.map(x=> documentToItem(x)))
-          ),
-          this.afs.collection('specializations').snapshotChanges().pipe(
-            take(1),
-            map( items => items.map(x=> documentToItem(x)))
-          ),
-          this.afs.collection('qualifications').snapshotChanges().pipe(
-            take(1),
-            map( items => items.map(x=> documentToItem(x)))
-          ),
-          this.afs.collection('skills').snapshotChanges().pipe(
-            take(1),
-            map( items => items.map(x=> documentToItem(x)))
-          )
-        ).pipe(/* Toda la data anterior esta siendo modificada por este pipe */
-          map(([roles,specializations,qualifications,skills])=>{
+          this.afs
+            .collection('roles')
+            .snapshotChanges()
+            .pipe(
+              take(1),
+              map((items) => items.map((x) => documentToItem(x)))
+            ),
+          this.afs
+            .collection('specializations')
+            .snapshotChanges()
+            .pipe(
+              take(1),
+              map((items) => items.map((x) => documentToItem(x)))
+            ),
+          this.afs
+            .collection('qualifications')
+            .snapshotChanges()
+            .pipe(
+              take(1),
+              map((items) => items.map((x) => documentToItem(x)))
+            ),
+          this.afs
+            .collection('skills')
+            .snapshotChanges()
+            .pipe(
+              take(1),
+              map((items) => items.map((x) => documentToItem(x)))
+            )
+        ).pipe(/* Toda la data anterior estara siendo modificada por este pipe */
+          map(([roles, specializations, qualifications, skills]) => {
             const dictionaries: Dictionaries = {
               roles: addDictionary(roles),
               specializations: addDictionary(specializations),
@@ -62,9 +86,10 @@ export class DictionariesEffects {
             };
             return new fromActions.ReadSuccess(dictionaries);
           }),
-          catchError(err => of(new fromActions.ReadError(err.message)))
-        )
+          /* En caso de error */
+          catchError((err) => of(new fromActions.ReadError(err.message)))
+        );
       })
     )
-  )
+  );
 }
